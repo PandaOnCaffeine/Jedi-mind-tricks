edf7e2052226ac45b12a6a00d572968e507d6710
zbctuma <zbctuma@ZBC-AA-DANI92A5>
2022-09-02 23:39:00 +0200

    Add project files.

---
+++ Jedi mind tricks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace Jedi_mind_tricks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates Dictionary
+            Dictionary<string, int> people = new Dictionary<string, int>();
+
+            //Adds "Jens" at the age of 50
+            people.Add("Jens", 50);
+            //Adds "Jack" at the age of 14
+            people["Jack"] = 14;
+
+            //Writes out each keyvaluepair in Dictionary people
+            foreach (KeyValuePair<string, int> k in people)
+            {
+                Console.WriteLine(k);
+            }
+
+            //Creates Dictionary and add "luke, Han and Chewbacca"
+            Dictionary<string, bool> characthers = new Dictionary<string, bool>()
+            {
+                { "Luke", true},
+                { "Han", false},
+                {"ChewBacca", false}
+            };
+
+            //Removes "Han"
+            characthers.Remove("Han");
+
+            //Writes each KeyValuePair in Dictionary Characthers
+            foreach (KeyValuePair<string,bool> k in characthers)
+            {
+                Console.WriteLine(k);
+            }
+
+            //So console doesn't close
+            Console.ReadLine();
+        }
+    }
+}
